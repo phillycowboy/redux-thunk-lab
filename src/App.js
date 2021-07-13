6d951036d8359e3252380928db282f31f1382754
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-07-12 21:07:59 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -8,13 +8,22 @@ class App extends Component {
     console.log(this.props)
     this.props.fetchCats()
   }
+  handleLoading = () => {
+    console.log(this.props.loading)
+    if(this.props.loading){
+      return <div>Loading...</div>
+    } else {
+      return  <CatList catPics={this.props.catPics}/>
+    }
+  }
+
   render() {
     console.log(this.props.catPics)
     return (
       <div>
         <h1>CatBook</h1>
         {/* add CatList component here */}
-        <CatList catPics={this.props.catPics}/>
+        {this.handleLoading()}
       </div>
     );
   }
